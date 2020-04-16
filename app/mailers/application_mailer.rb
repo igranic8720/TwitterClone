f0405019b8ab0aee74477d2df92712072014f1a3
Ubuntu <ubuntu@ip-172-31-11-190.us-east-2.compute.internal>
2020-04-16 22:33:23 +0000

    fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'ivan.granic.ig@gmail.com'
+  default from: 'postmaster@sandbox3ce06d281b364d3480f7d65b27f404fc.mailgun.org'
   layout 'mailer'
 end
