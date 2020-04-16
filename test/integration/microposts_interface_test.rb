b6c7b5ef830494cac869591921d6ec78738b2a47
Ubuntu <ubuntu@ip-172-31-11-190.us-east-2.compute.internal>
2020-04-16 19:57:53 +0000

    add user following

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -34,9 +34,9 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
-    # Visit a different user.
+    # Visit different user (no delete links)
     get user_path(users(:archer))
-    assert_select 'a', { text: 'delete', count: 0 }
+    assert_select 'a', text: 'delete', count: 0
   end
   
   test "micropost sidebar count" do
